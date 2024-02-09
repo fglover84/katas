800d4de2c74aa039d9c424a7c905eedbd8dd7561
fglover84 <fiona.glover@bbc.co.uk>
2024-02-09 15:17:29 +0000

    word count

--- src/Exercism/word-count.ts
+++ src/Exercism/word-count.ts
@@ -1,3 +1,36 @@
-export function count() {
- 
+export function count(input: string) {
+    const specialChars = "!&@$%^&\":?."
+    const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    // "one,two,three"
+    const inputArray: string[] = input.split(" ").join().split("\n").join().split(", ").join().split(",");
+    // ["one", "of", "each"]
+    const inputMap = new Map<string, number>()
+
+    inputArray.map((item) => {
+        let sanitizedItem = item.split("").filter((char) => specialChars.indexOf(char) == -1).join("").toLowerCase()
+        const itemArray = sanitizedItem.split("");
+        for (let i = 0; i < itemArray.length; i++){
+            if (itemArray[i] == "'" && i == 0) {
+                itemArray[0] = "";
+            } else {
+                if (itemArray[i] == "'") {
+                    if (alpha.indexOf(itemArray[i - 1]) == -1 || alpha.indexOf(itemArray[i + 1]) == -1) {
+                        itemArray[i] = "";
+                    }
+                }
+            }
+        }
+        sanitizedItem = itemArray.join("");
+        if (item.length > 0) {
+            const itemValue = inputMap.get(sanitizedItem)
+            if (!itemValue) {
+                inputMap.set(sanitizedItem, 1)
+            } else {
+                inputMap.set(sanitizedItem, itemValue + 1)
+            }
+        }
+        
+    })
+    return inputMap;
 }
